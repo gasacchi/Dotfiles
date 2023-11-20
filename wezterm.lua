10297d49e937ea42e28897e9e0e6d42ec7bb19ee
gasacchi <gasacchi404@gmail.com>
2023-11-21 01:16:28 +0700

    move tab bar

--- wezterm.lua
+++ wezterm.lua
@@ -7,9 +7,11 @@ if wezterm.config_builder then
 end
 
 
-config.color_scheme = 'GitHub Dark'
+config.color_scheme = "GitHub Dark"
 config.font = wezterm.font "JetBrainsMono Nerd Font"
 config.font_size = 9
-
+config.front_end = "WebGpu"
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
 
 return config
