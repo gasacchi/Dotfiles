f93f25134133f750caa5338cc3a6f30ba2260db7
gasacchi <gasacchi404@gmail.com>
2023-12-13 00:16:55 +0700

    wip ide like

--- fish/config.fish
+++ fish/config.fish
@@ -13,11 +13,11 @@ alias rm="rm -v"
 alias mv="mv -v"
 alias cp="cp -v"
 alias ln="ln -v"
+alias z=zellij
 
 alias cat=bat
 
 set -gx HELIX_RUNTIME "$HOME/.local/helix/runtime"
-
 set -gx EDITOR "helix"
 
 # local bin
@@ -28,13 +28,15 @@ set -gx PATH "$HOME/.local/bin:$PATH"
 # rust path
 set -gx PATH "$HOME/.cargo/bin:$PATH"
 
-## bun
-set -gx PATH "$HOME/.bun/bin:$PATH"
+# ## bun
+# set -gx PATH "$HOME/.bun/bin:$PATH"
 
 # if status is-interactive
 #     # Commands to run in interactive sessions can go here
 # end
 
+# call gruvbox function
+theme_gruvbox dark hard
 
 starship init fish | source
 
